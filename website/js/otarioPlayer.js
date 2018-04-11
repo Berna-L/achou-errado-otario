29755efce7baedec87d08005196aab8c027b0443
Bernardo Lopes <bernardo.lopes@outlook.com>
2018-04-11 20:55:48 -0300

    Nome e link para vídeo completo.

--- website/js/otarioPlayer.js
+++ website/js/otarioPlayer.js
@@ -1,11 +1,13 @@
 $(function () {
     let videoPlayer = $("#bgndVideo");
     let videoJson = videoPlayer.data("property");
-    let possibleVideos = JSON.parse(document.getElementById("video-list").innerHTML).videos;
+    let possibleVideos = JSON.parse(videoList).videos;
     let chosenVideo = possibleVideos[Math.floor(Math.random() * possibleVideos.length)];
     videoJson.videoURL = chosenVideo.url;
     videoJson.startAt = chosenVideo.startAt;
     videoJson.stopAt = chosenVideo.stopAt;
     videoPlayer.attr('data-property', JSON.stringify(videoJson));
+    $("#nome-video").text(chosenVideo.description);
+    $("#link-video").attr('href', chosenVideo.url);
     videoPlayer.YTPlayer();
 });
---
+++ website/js/video-list.js
@@ -0,0 +1,18 @@
+const videoList = `
+{
+  "videos": [
+    {
+      "url": "https://www.youtube.com/watch?v=s5-fdzY1JGw",
+      "description": "CHOQUE DE CULTURA #3: Velozes e Furiosos é Arte",
+      "startAt": 5,
+      "stopAt": 9.5
+    },
+    {
+      "url": "https://www.youtube.com/watch?v=oo8iJFaK_yM",
+      "description": "STRANGER THINGS É NOVELA DE CRIANÇA | Choque de Cultura",
+      "startAt": -1,
+      "stopAt": 4.2
+    }
+  ]
+}
+`;
